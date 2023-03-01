185dd4d3a3a6f51bd341c28d773b7b88d0c88ddd
seunjay <seunjay92@gmail.com>
2023-03-01 10:19:28 +0100

    css styles fixes

--- src/index.js
+++ src/index.js
@@ -3,12 +3,11 @@ import ReactDOM from "react-dom";
 import { ChakraProvider } from "@chakra-ui/react";
 import App from "./App";
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <StrictMode>
     <ChakraProvider>
       <App />
     </ChakraProvider>
-  </StrictMode>,
-  rootElement
+  </StrictMode>
 );
