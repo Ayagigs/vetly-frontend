631ee99a467376c0c277f56477183da408fbb066
Wutche Chikaome <wutchechikaome75@gmail.com>
2023-03-03 19:38:00 +0100

    made some changes

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from "./pages/home/Home";
 import Signup from "./pages/signup/Signup";
 import Signin from "./pages/signin/Signin";
 import Verifyaccount from "./pages/verifyaccount/Verifyaccount";
+import ForgotPassword from "./pages/forgot-password/ForgotPassword";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/sign-up" element={<Signup />} />
       <Route path="/signin" element={<Signin />} />
+			<Route path = "/forgotten-password" element = {<ForgotPassword />} />
       <Route path="/verify-account" element={<Verifyaccount />} />
     </Routes>
   );
