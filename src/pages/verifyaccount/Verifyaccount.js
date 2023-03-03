d4559609312983a2cf7574ffcee7fb414dc069ad
seunjay <seunjay92@gmail.com>
2023-03-03 13:52:51 +0100

    refactor css

--- src/pages/verifyaccount/Verifyaccount.js
+++ src/pages/verifyaccount/Verifyaccount.js
@@ -3,30 +3,23 @@ import "./verifyaccount.css";
 import verifylogo from "../../assets/verifylogo.png";
 import { Link } from "@chakra-ui/react";
 
-
-
-
 const Verifyaccount = () => {
-    return (
-        <div className="verify-parent">
-            <div className="verifylogo-container">
-                <img src={verifylogo} alt="verifylogo" />
-            </div>
-            <div className="heading-container">
-                <h3>Verify your account</h3>
-            </div>
-            <div className="body-container">
-                Kindly check your email for a link to verify your account
-
-            </div>
-            <div className="mail-container">
-                Didn't get a mail? <Link>Resend</Link>
-            </div>
-        </div>
-
-
-
-    );
-}
+  return (
+    <div className="verify-parent">
+      <div className="center verifylogo-container">
+        <img src={verifylogo} alt="verifylogo" />
+      </div>
+      <div className="center heading-container">
+        <h3>Verify your account</h3>
+      </div>
+      <div className="center body-container">
+        Kindly check your email for a link to verify your account
+      </div>
+      <div className="center mail-container">
+        Didn't get a mail? <Link>Resend</Link>
+      </div>
+    </div>
+  );
+};
 
 export default Verifyaccount;
