a969b742494bd9c096a32f0441e3cc359b0a7061
Wutche Chikaome <wutchechikaome75@gmail.com>
2023-03-03 18:36:42 +0100

    little change

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Signup from "./pages/signup/Signup";
 import Signin from "./pages/signin/Signin";
-import Verify from "./pages/wronginput/verify";
+import ForgotPassword from "./pages/forgot-password/verify";
 
 function App() {
 	return (
@@ -12,7 +12,7 @@ function App() {
 			<Route path="/" element={<Home />} />
 			<Route path="/sign-up" element={<Signup />} />
 			<Route path="/signin" element={<Signin />} />
-			<Route path = "/verify" element={<Verify />} />
+			<Route path="/verify" element={<ForgotPassword />} />
 		</Routes>
 	);
 }
