34c2e34c885a9f5987a17c048e59328e3ee390f9
seunjay <seunjay92@gmail.com>
2023-03-03 14:16:23 +0100

    resolving conflicts

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
-/** @format */
-
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Signup from "./pages/signup/Signup";
 import Signin from "./pages/signin/Signin";
-import Verify from "./pages/verification/verify";
+import Verifyaccount from "./pages/verifyaccount/Verifyaccount";
 
 function App() {
-	return (
-		<Routes>
-			<Route path="/" element={<Home />} />
-			<Route path="/sign-up" element={<Signup />} />
-			<Route path="/signin" element={<Signin />} />
-			<Route path = "/verify" element={<Verify />} />
-		</Routes>
-	);
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/sign-up" element={<Signup />} />
+      <Route path="/signin" element={<Signin />} />
+      <Route path="/verify-account" element={<Verifyaccount />} />
+    </Routes>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 // import ReactDOM from "react-dom";
 import * as ReactDOMClient from "react-dom/client";
-
 import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
