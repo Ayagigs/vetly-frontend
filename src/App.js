50d70fef1551263ea9fb30e1f548bd6934c521bf
Wutche Chikaome <wutchechikaome75@gmail.com>
2023-03-03 01:50:29 +0100

    done with forgotten password page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Signup from "./pages/signup/Signup";
 import Signin from "./pages/signin/Signin";
+import Verify from "./pages/verification/verify";
 
 function App() {
 	return (
@@ -11,6 +12,7 @@ function App() {
 			<Route path="/" element={<Home />} />
 			<Route path="/sign-up" element={<Signup />} />
 			<Route path="/signin" element={<Signin />} />
+			<Route path = "/verify" element={<Verify />} />
 		</Routes>
 	);
 }
