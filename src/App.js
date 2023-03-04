304d844e5c77e3302faa17323b9a5d0fa0451813
pheobeayo <47613365+pheobeayo@users.noreply.github.com>
2023-03-04 11:55:55 +0100

    Reset Password
    This branch will reset the password when a user forgets his or her password.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from "./pages/home/Home";
 import Signup from "./pages/signup/Signup";
 import Signin from "./pages/signin/Signin";
 import Verifyaccount from "./pages/verifyaccount/Verifyaccount";
+import Resetpassword from "./pages/resetpassword/Resetpassword";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Route path="/sign-up" element={<Signup />} />
       <Route path="/signin" element={<Signin />} />
       <Route path="/verify-account" element={<Verifyaccount />} />
+      <Route path="/reset-password" element={<Resetpassword/>} />
     </Routes>
   );
 }
