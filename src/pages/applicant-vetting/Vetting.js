9217717dac15f563bf9929adc24003dbca2cf16f
seunjay <seunjay92@gmail.com>
2023-03-08 14:12:58 +0100

    vetting module

--- src/pages/applicant-vetting/Vetting.js
+++ src/pages/applicant-vetting/Vetting.js
@@ -1,18 +1,148 @@
+import { useEffect, useState } from "react";
 import {
   ApplicantVettingParent,
   CenterWrapper,
+  DataHeader,
+  DataHeadersContainer,
+  DataRowsContainer,
+  DateContainer,
+  EmailContainer,
+  EmployerContainer,
+  Header,
+  Rows,
+  StatusBadge,
+  StatusContainer,
   VettingHeadersContainer,
+  Wrapper,
 } from "./applicant.vetting.styles";
+import avatar from "../../assets/avatar.png";
+
+const headers = [
+  {
+    id: 0,
+    name: "Successful Vetting",
+  },
+  {
+    id: 1,
+    name: "Pending Requests",
+  },
+  {
+    id: 2,
+    name: "Denied Requests",
+  },
+];
+
+const data = [
+  {
+    id: 0,
+    name: "Oluwaseun Jayeoba",
+    date: "March 8, 2023",
+    email: "seunjay92@gmail.com",
+    status: "pending",
+    avatar: "../../assets/avatar.png",
+  },
+  {
+    id: 1,
+    name: "Oluwaseun Jayeoba",
+    date: "March 8, 2023",
+    email: "seunjay92@gmail.com",
+    status: "success",
+    avatar: "../../assets/avatar.png",
+  },
+  {
+    id: 2,
+    name: "Oluwaseun Jayeoba",
+    date: "March 8, 2023",
+    email: "seunjay92@gmail.com",
+    status: "declined",
+    avatar: "../../assets/avatar.png",
+  },
+  {
+    id: 3,
+    name: "Oluwaseun Jayeoba",
+    date: "March 8, 2023",
+    email: "seunjay92@gmail.com",
+    status: "success",
+    avatar: "../../assets/avatar.png",
+  },
+  {
+    id: 4,
+    name: "Oluwaseun Jayeoba",
+    date: "March 8, 2023",
+    email: "seunjay92@gmail.com",
+    status: "declined",
+    avatar: "../../assets/avatar.png",
+  },
+  {
+    id: 5,
+    name: "Oluwaseun Jayeoba",
+    date: "March 8, 2023",
+    email: "seunjay92@gmail.com",
+    status: "pending",
+    avatar: "../../assets/avatar.png",
+  },
+];
+
+const filter = (statuscode = 0) => {
+  const status =
+    statuscode === 0 ? "success" : statuscode === 1 ? "pending" : "declined";
+
+  return data.filter((el) => el.status === status);
+};
 
 const Vetting = () => {
+  const [activeHeader, setActiveHeader] = useState(0);
+  const [vettingData, setVettingData] = useState([]);
+
+  useEffect(() => {
+    const data = filter(activeHeader);
+    setVettingData(data);
+  }, [activeHeader]);
+
+
   return (
     <ApplicantVettingParent>
       <CenterWrapper>
         <VettingHeadersContainer>
-          <h1>Successful Vetting</h1>
-          <h1>Pending Requests</h1>
-          <h1>Denied Requests</h1>
+          {headers.map((header) => (
+            <Header
+              active={+(activeHeader === header.id)}
+              key={header.id}
+              onClick={() => setActiveHeader(header.id)}
+            >
+              {header.name}
+            </Header>
+          ))}
         </VettingHeadersContainer>
+
+        <DataHeadersContainer>
+          <DataHeader>Employer</DataHeader>
+          <DataHeader>Date</DataHeader>
+          <DataHeader>Status</DataHeader>
+          <DataHeader>Email</DataHeader>
+        </DataHeadersContainer>
+
+        <DataRowsContainer>
+          <Wrapper>
+            {vettingData.map((el) => (
+              <Rows key={el.id}>
+                <EmployerContainer>
+                  <img src={avatar} alt="avatar" className="employee-avatar" />
+                  <h1 className="employee-name">{el.name}</h1>
+                </EmployerContainer>
+                <DateContainer>
+                  <p className="date">{el.date}</p>
+                </DateContainer>
+                <StatusContainer>
+                  <StatusBadge status={el.status}>{el.status}</StatusBadge>
+                </StatusContainer>
+                <EmailContainer>
+                  <p className="email">{el.email}</p>
+                </EmailContainer>
+              </Rows>
+            ))}
+          </Wrapper>
+        </DataRowsContainer>
       </CenterWrapper>
     </ApplicantVettingParent>
   );
--- src/pages/applicant-vetting/applicant.vetting.styles.js
+++ src/pages/applicant-vetting/applicant.vetting.styles.js
@@ -17,18 +17,151 @@ export const VettingHeadersContainer = styled.section`
   align-items: center;
   width: 100%;
   height: 5.8rem;
-  padding: 0 2rem;
+  padding: 0 3rem;
   background: #ffffff;
-  border-top: 1px solid #F1EEED;
-  border-right: 1px solid #F1EEED;
-  border-left: 1px solid #F1EEED;
+  border-top: 1px solid #f1eeed;
+  border-right: 1px solid #f1eeed;
+  border-left: 1px solid #f1eeed;
   border-bottom: 5px solid #cde2fe;
   border-radius: 8px;
+`;
 
-  & h1 {
+export const Header = styled.h1`
+  font-weight: 500;
+  font-size: 1.4rem;
+  line-height: 28px;
+  text-transform: uppercase;
+  cursor: pointer;
+  border-bottom: ${(props) => {
+    return props.active === 1 ? "4px solid #0570FB" : "none";
+  }};
+`;
+
+export const DataHeadersContainer = styled.section`
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  background: #fbfbfb;
+  padding: 0px 30px;
+  height: 5.8rem;
+  width: 100%;
+  gap: 5rem;
+`;
+
+export const DataHeader = styled.div`
+  padding: 0px 24px;
+  box-shadow: inset 0px -1px 0px #edf2f7;
+  width: 15%;
+  display: flex;
+  justify-content: center;
+  font-weight: 500;
+  font-size: 1.5rem;
+  line-height: 2.8rem;
+`;
+
+export const DataRowsContainer = styled.section`
+  background: #ffffff;
+  width: 100%;
+  height: auto;
+  border: 1px solid red;
+  border: 1px solid rgba(1, 22, 50, 0.1);
+  box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.05), 0px 25px 35px rgba(0, 0, 0, 0.03);
+`;
+
+export const Wrapper = styled.div`
+  margin: 0 auto;
+  width: 90%;
+  height: 100%;
+`;
+
+export const Rows = styled.section`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  height: 9.3rem;
+  width: 100%;
+  gap: 2rem;
+  box-shadow: inset 0px -1px 0px #edf2f7;
+`;
+
+export const EmployerContainer = styled.div`
+  display: flex;
+  gap: 1rem;
+  flex: 20%;
+  height: 100%;
+  align-items: center;
+  padding: 1rem;
+
+  & .employee-avatar {
+    width: 4.8rem;
+  }
+  & .employee-name {
+    color: #5d5e5f;
     font-weight: 500;
-    font-size: 18px;
-    line-height: 28px;
-    text-transform: uppercase;
+    font-size: 1.2rem;
+    line-height: 18px;
+  }
+`;
+
+export const DateContainer = styled.div`
+  display: flex;
+  flex: 10%;
+  padding-left: 1rem;
+  height: 100%;
+  align-items: center;
+
+  & .date {
+    font-weight: 500;
+    font-size: 1.2rem;
+    line-height: 18px;
+    color: #5d5e5f;
+  }
+`;
+
+export const StatusContainer = styled.div`
+  display: flex;
+  flex: 13%;
+  height: 100%;
+  padding-left: 2rem;
+  align-items: center;
+`;
+
+// #fff9e5
+
+export const StatusBadge = styled.div`
+  background: ${(props) =>
+    props.status === "success"
+      ? "#F2FCF9"
+      : props.status === "declined"
+      ? "#FEF1F1"
+      : "#fff9e5"};
+  border-radius: 100px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding: 12px;
+  width: 158px;
+  height: 60px;
+  color: ${(props) =>
+    props.status === "success"
+      ? "#22A57E"
+      : props.status === "declined"
+      ? "#B21015"
+      : "#BFA24C"};
+`;
+
+export const EmailContainer = styled.div`
+  display: flex;
+  flex: 10%;
+  height: 100%;
+  align-items: center;
+  padding-left: 4rem;
+
+  & .email {
+    font-weight: 500;
+    font-size: 1.2rem;
+    line-height: 18px;
+    color: #5d5e5f;
   }
 `;
