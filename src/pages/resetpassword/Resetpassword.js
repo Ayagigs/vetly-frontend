79496d97609fcc43e4b7785b342a815f837a47fa
seunjay <seunjay92@gmail.com>
2023-03-11 22:23:20 +0100

    fix reset password ui issues

--- src/pages/resetpassword/Resetpassword.js
+++ src/pages/resetpassword/Resetpassword.js
@@ -1,96 +1,80 @@
 import React from "react";
 import "./resetpassword.css";
 import logo from "../../assets/logo.png";
-import {
-    FormControl,
-    Text,
-    FormLabel,
-    Input,
-    Button,
-    Link
-} from "@chakra-ui/react";
+import { FormControl, Text, FormLabel, Input, Button } from "@chakra-ui/react";
+import { NavLink } from "react-router-dom";
 
 const Resetpassword = () => {
-    return (
-        <div className="reset-parent">
-            <div className="logo-container">
-                <img src={logo} alt="logo" />
-                <h3>Vetly</h3>
-            </div>
-            <div className="form-container">
-                <form>
-                    <div className="form-header">
-                        Reset your password
+  return (
+    <div className="reset-parent">
+      <div className="logo-container">
+        <img src={logo} alt="logo" />
+        <h3>Vetly</h3>
+      </div>
+      <div className="form-container">
+        <form>
+          <div className="form-header">Reset your password</div>
+          <Text
+            fontWeight="400"
+            fontSize="15"
+            textAlign="center"
+            marginBottom="25"
+            color={"#5D5E5F"}
+          >
+            Enter your new password
+          </Text>
 
+          <FormControl marginBottom={"1rem"}>
+            <FormLabel
+              fontSize="12"
+              fontWeight="500"
+              fontFamily="inherit"
+              paddingBlock="2px"
+            >
+              New password
+            </FormLabel>
+            <Input
+              type="password"
+              fontSize="12"
+              fontWeight="400"
+              fontFamily="inherit"
+              placeholder="**********"
+              height={"3.2rem"}
+            />
+          </FormControl>
 
-                    </div>
-                    <Text
-                        fontWeight="400"
-                        fontSize="20"
-                        textAlign="center"
-                        marginBottom="25"
-                    >
+          <FormControl marginBottom={"1rem"}>
+            <FormLabel fontSize="12" fontWeight="500" fontFamily="inherit">
+              Confirm new password
+            </FormLabel>
+            <Input
+              type="password"
+              fontSize="12"
+              fontWeight="400"
+              fontFamily="inherit"
+              placeholder="**********"
+              height={"3.2rem"}
+            />
+          </FormControl>
 
-                        Enter your new password
-                    </Text>
-
-                    <FormControl>
-                        <FormLabel
-                            fontSize="12"
-                            fontWeight="500"
-                            fontFamily="inherit"
-                            paddingBlock="2px">
-                            New password
-                        </FormLabel>
-                        <Input
-                            type="password"
-                            fontSize="12"
-                            fontWeight="400"
-                            fontFamily="inherit"
-                            placeholder="**********"
-                        />
-                    </FormControl>
-
-                    <FormControl>
-                        <FormLabel
-                            fontSize="12"
-                            fontWeight="500"
-                            fontFamily="inherit"
-                        >
-                            Confirm new password
-                        </FormLabel>
-                        <Input
-                            type="password"
-                            fontSize="12"
-                            fontWeight="400"
-                            fontFamily="inherit"
-                            placeholder="**********"
-                        />
-                    </FormControl>
-
-
-                    <Button
-                        colorScheme="blue"
-                        width="full"
-                        mt={4}
-                        fontSize="12"
-                        fontWeight="500"
-                        fontFamily="inherit"
-                    >
-                        Reset your password
-                    </Button>
-                    <Link
-                        fontFamily="inherit"
-                        paddingTop="10"
-                        fontSize="14"
-                    >
-                        Return to login page</Link>
-                </form>
-            </div>
-        </div>
-
-
-    );
+          <Button
+            colorScheme={"blue"}
+            color={"#fff"}
+            width="full"
+            mt={4}
+            fontSize="12"
+            fontWeight="500"
+            fontFamily="inherit"
+            backgroundColor={"#0570FB"}
+            height={"3.2rem"}
+          >
+            Reset your password
+          </Button>
+          <NavLink to="/sign-in" className="navigate">Return to login page</NavLink>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Resetpassword;
