75da7cec728de4bfc9d620dadf0f52b60eea673e
wutche <wutche@pop-os.localdomain>
2023-03-15 17:01:51 +0100

    done wth fetching the signin api

--- src/components/button.js
+++
@@ -1,5 +0,0 @@
-const Button = () => {
-  return <button>Sign in</button>
-}
-
-export default Button
--- src/components/form.js
+++ src/components/form.js
@@ -1,26 +1,72 @@
 /** @format */
-import Finput from "./Finputs";
-import Button from "./button";
 import Social from "./social";
-import Data from "./Data";
+import axios from "axios";
+import { useState } from "react";
 const Form = () => {
-  const inputs = Data.map((item) => {
-    return <Finput key={item.id} {...item} />;
-  });
-  return (
-    <form action="" className="signin-form">
-      <div className="formtop">
-        <div className="header">
-          <h1>Sign in</h1>
-          <p>Welcome back! Please enter your details.</p>
-        </div>
-        {inputs}
-        <Button />
-      </div>
-      <div className="formbottom">
-        <Social />
-      </div>
-    </form>
-  );
+	const url =
+		"https://vetly-backend-production.up.railway.app/api/v1/auth/login";
+
+	const [data, setData] = useState({
+		email: "",
+		password: "",
+		user_type: "applicant"
+	});
+
+	const submit = (e) => {
+		e.preventDefault();
+		axios
+			.post(url, data)
+			.then((res) => {
+				console.log(res.data);
+			});
+		console.log(data);
+	};
+
+	const postUser = (e) => {
+		const newData = { ...data };
+		newData[e.target.id] = e.target.value;
+		setData(newData);
+		console.log(newData);
+	};
+
+	return (
+		<form action="" className="signin-form" onSubmit={(e) => submit(e)}>
+			<div className="formtop">
+				<div className="header">
+					<h1>Sign in</h1>
+					<p>Welcome back! Please enter your details.</p>
+				</div>
+				{/* Email input element*/}
+				<label className="form-label">
+					<h2>Email</h2>
+				</label>
+				<input
+					className="form-label"
+					onChange={(e) => postUser(e)}
+					id="email"
+					value={data.email}
+					type="email"
+					placeholder="Enter email"
+					required
+				/>
+				{/* password=input element */}
+				<label className="form-label">
+					<h2>Password</h2>
+				</label>
+				<input
+					className="form-label"
+					onChange={(e) => postUser(e)}
+					id="password"
+					value={data.password}
+					type="password"
+					placeholder="Enter password"
+				/>
+				<button>Sign in</button>
+			</div>
+			<div className="formbottom">
+				<Social />
+			</div>
+		</form>
+	);
 };
 export default Form;
