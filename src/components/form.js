9ce888d1f9fd1bdf72eaf7c3ff8eb16f3e0ecaa4
seunjay <seunjay92@gmail.com>
2023-03-16 10:48:53 +0100

    route user to dashboard upon signing in

--- src/components/form.js
+++ src/components/form.js
@@ -1,70 +1,106 @@
 /** @format */
 import Social from "./social";
-import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Box, Spinner, useToast } from "@chakra-ui/react";
+
+import { APIConfig } from "../config/apiConfig";
+
 const Form = () => {
-	const url =
-		"https://vetly-backend-production.up.railway.app/api/v1/auth/login";
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+    user_type: "applicant",
+  });
+  const [loading, setLoading] = useState(false);
+  const [success, setSuccess] = useState(false);
 
-	const [data, setData] = useState({
-		email: "",
-		password: "",
-		user_type: "applicant",
-	});
+  const navigate = useNavigate();
+  const toast = useToast();
 
-	const submit = (e) => {
-		e.preventDefault();
-		axios.post(url, data).then((res) => {
-			return res.data;
-		});
-		return data;
-	};
+  useEffect(() => {
+    if (success) {
+      toast({
+        position: "top-left",
+        render: () => (
+          <Box color="white" p={3} bg="green.500" fontSize={15}>
+            Sign in successful!
+          </Box>
+        ),
+        onCloseComplete: () => {
+          navigate("/applicant/");
+        },
+      });
+    }
+  }, [success, navigate, toast]);
 
-	const postUser = (e) => {
-		const newData = { ...data };
-		newData[e.target.id] = e.target.value;
-		setData(newData);
-		return newData;
-	};
+  const { email, password } = form;
 
-	return (
-		<form action="" className="signin-form" onSubmit={(e) => submit(e)}>
-			<div className="formtop">
-				<div className="header">
-					<h1>Sign in</h1>
-					<p>Welcome back! Please enter your details.</p>
-				</div>
-				{/* Email input element*/}
-				<label className="form-label">
-					<h2>Email</h2>
-				</label>
-				<input
-					className="form-label"
-					onChange={(e) => postUser(e)}
-					id="email"
-					value={data.email}
-					type="email"
-					placeholder="Enter email"
-					required
-				/>
-				{/* password=input element */}
-				<label className="form-label">
-					<h2>Password</h2>
-				</label>
-				<input
-					className="form-label"
-					onChange={(e) => postUser(e)}
-					id="password"
-					value={data.password}
-					type="password"
-					placeholder="Enter password"
-				/>
-				<button>Sign in</button>
-			</div>
-			<div className="formbottom">
-				<Social />
-			</div>
-		</form>
-	);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    try {
+      const { data } = await APIConfig.post("auth/login", form);
+      setLoading(false);
+      setSuccess(true);
+      localStorage.setItem("currentUser", JSON.stringify(data?.user));
+      localStorage.setItem("userToken", data?.token);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setForm((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  return (
+    <form action="" className="signin-form" onSubmit={handleSubmit}>
+      <div className="formtop">
+        <div className="header">
+          <h1>Sign in</h1>
+          <p>Welcome back! Please enter your details.</p>
+        </div>
+        {/* Email input element*/}
+        <label className="form-label">
+          <h2>Email</h2>
+        </label>
+        <input
+          className="form-label"
+          onChange={handleChange}
+          id="email"
+          name="email"
+          value={email}
+          type="email"
+          placeholder="Enter email"
+          required
+        />
+        {/* password=input element */}
+        <label className="form-label">
+          <h2>Password</h2>
+        </label>
+        <input
+          className="form-label"
+          onChange={handleChange}
+          id="password"
+          name="password"
+          value={password}
+          type="password"
+          placeholder="Enter password"
+        />
+        <button>
+          {!loading ? "Sign in" : <Spinner size="sm" color="white.500" />}
+        </button>
+      </div>
+      <div className="formbottom">
+        <Social />
+      </div>
+    </form>
+  );
 };
 export default Form;
