165dacf72cbb83bc6886a7b385f433d1fa2de247
wutche <wutche@pop-os.localdomain>
2023-03-24 22:57:19 +0100

    made some changes

--- src/pages/registered-companies/RegisteredCompanies.js
+++ src/pages/registered-companies/RegisteredCompanies.js
@@ -1,8 +1,6 @@
 /** @format */
 
-// import React, { useState } from "react";
-import "./registeredcomp.css";
-import { CompanyTableSelection } from "../../components/registeredcomany/registeredCompanycomp";
+
 const RegisteredCompanies = () => {
 
 	// const [isactive, setIsactive] = useState(true)
@@ -10,8 +8,7 @@ const RegisteredCompanies = () => {
 	return (
 		<div className="registeredcomp">
 			<div className="registeredheader">
-				<h1>Vetly Team Members</h1>
-				<button>Verify Member</button>
+				<h1>Registered Companies</h1>
 			</div>
 			<input
 				type="text"
@@ -19,48 +16,7 @@ const RegisteredCompanies = () => {
 				className="registeredinput"
 			/>
 
-			<div className="registeredcomptable">
-				<CompanyTableSelection
-					data={[
-						{
-							id: 0,
-							name: "Fatima Aminu",
-							avatar:
-								"https://images.unsplash.com/photo-1624298357597-fd92dfbec01d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=250&q=80",
-							email: "Teemah@vetly.com",
-							date: "20th February, 2023",
-							status: true,
-						},
-						{
-							id: 1,
-							name: "Fatima Aminu",
-							avatar:
-								"https://images.unsplash.com/photo-1624298357597-fd92dfbec01d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=250&q=80",
-							email: "Teemah@vetly.com",
-							date: "20th February, 2023",
-							status: false,
-						},
-						{
-							id: 2,
-							name: "Fatima Aminu",
-							avatar:
-								"https://images.unsplash.com/photo-1624298357597-fd92dfbec01d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=250&q=80",
-							email: "Teemah@vetly.com",
-							date: "20th February, 2023",
-							status: true,
-						},
-						{
-							id: 3,
-							name: "Fatima Aminu",
-							avatar:
-								"https://images.unsplash.com/photo-1624298357597-fd92dfbec01d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=250&q=80",
-							email: "Teemah@vetly.com",
-							date: "20th February, 2023",
-							status: true,
-						},
-					]}
-				/>
-			</div>
+
 		</div>
 	);
 };
--- src/pages/team-members/TeamMembers.js
+++ src/pages/team-members/TeamMembers.js
@@ -1,9 +1,67 @@
-import React from 'react'
+/** @format */
 
-const TeamMembers = () => {
-  return (
-    <div>TeamMembers</div>
-  )
-}
+// import React, { useState } from "react";
+import "./teamMember.css";
+import { CompanyTableSelection } from "../../components/Team-membercomp/Teammembercomp";
+const RegisteredCompanies = () => {
+	// const [isactive, setIsactive] = useState(true)
+	// setIsactive(prevactive => !prevactive)
+	return (
+		<div className="registeredcomp">
+			<div className="registeredheader">
+				<h1>Vetly Team Members</h1>
+				<button>Verify Member</button>
+			</div>
+			<input
+				type="text"
+				placeholder="Search by name or email"
+				className="registeredinput"
+			/>
 
-export default TeamMembers
+			<div className="registeredcomptable">
+				<CompanyTableSelection
+					data={[
+						{
+							id: 0,
+							name: "Fatima Aminu",
+							avatar:
+								"https://images.unsplash.com/photo-1624298357597-fd92dfbec01d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=250&q=80",
+							email: "Teemah@vetly.com",
+							date: "20th February, 2023",
+							status: true,
+						},
+						{
+							id: 1,
+							name: "Fatima Aminu",
+							avatar:
+								"https://images.unsplash.com/photo-1624298357597-fd92dfbec01d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=250&q=80",
+							email: "Teemah@vetly.com",
+							date: "20th February, 2023",
+							status: false,
+						},
+						{
+							id: 2,
+							name: "Fatima Aminu",
+							avatar:
+								"https://images.unsplash.com/photo-1624298357597-fd92dfbec01d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=250&q=80",
+							email: "Teemah@vetly.com",
+							date: "20th February, 2023",
+							status: true,
+						},
+						{
+							id: 3,
+							name: "Fatima Aminu",
+							avatar:
+								"https://images.unsplash.com/photo-1624298357597-fd92dfbec01d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=250&q=80",
+							email: "Teemah@vetly.com",
+							date: "20th February, 2023",
+							status: true,
+						},
+					]}
+				/>
+			</div>
+		</div>
+	);
+};
+
+export default RegisteredCompanies;
