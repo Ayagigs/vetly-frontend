39b57ae085ebeeb5f57d64e697cb68a13d71b9f1
seunjay <seunjay92@gmail.com>
2023-03-28 08:31:25 +0100

    companies search

--- src/pages/registered-companies/RegisteredCompanies.js
+++ src/pages/registered-companies/RegisteredCompanies.js
@@ -6,6 +6,7 @@ import { RegisteredCompTable } from "../../components/registeredcomptable/regist
 import { useEffect, useState } from "react";
 import { APIConfig } from "../../config/apiConfig";
 import { formatCompaniesData } from ".";
+import { Spinner } from "@chakra-ui/react";
 
 const RegisteredCompanies = () => {
   const [registeredCompanies, setRegisteredCompanies] = useState([]);
@@ -17,7 +18,33 @@ const RegisteredCompanies = () => {
     try {
       const { data } = await APIConfig.get("users?user_type=business");
       const formattedData = formatCompaniesData(data);
-      console.log(data);
+      setLoading(false);
+      setRegisteredCompanies(formattedData);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      if (searchTerm !== "") {
+        handleCompanySearch(searchTerm);
+      } else {
+        fetchCompanies();
+      }
+    }
+  };
+
+  const handleCompanySearch = async (keyWord) => {
+    setLoading(true);
+    try {
+      const { data } = await APIConfig.get(`users/search?keyword=${keyWord}`);
+      const formattedData = formatCompaniesData(data);
+
       setLoading(false);
       setRegisteredCompanies(formattedData);
     } catch (error) {
@@ -39,9 +66,32 @@ const RegisteredCompanies = () => {
         <h1>Registered Companies</h1>
       </div>
       <div className="carditems">{compCard}</div>
-      <input type="text" placeholder="Search by name" className="compinput" />
+      <input
+        type="text"
+        placeholder="Search by name"
+        className="compinput"
+        onChange={handleChange}
+        onKeyDown={handleKeyDown}
+      />
       <div className="registeredcmpnytable">
-        <RegisteredCompTable data={registeredCompanies} />
+        {loading ? (
+          <>
+            {" "}
+            <Spinner
+              thickness="2px"
+              speed="0.65s"
+              emptyColor="gray.200"
+              color="blue.500"
+              size="xl"
+              className="spinner"
+            />
+          </>
+        ) : (
+          <>
+            {" "}
+            <RegisteredCompTable data={registeredCompanies} />
+          </>
+        )}
       </div>
     </div>
   );
