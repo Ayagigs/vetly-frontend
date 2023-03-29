5f8e1032829268cbd34acceb1e04fce35506aefb
wutche <wutche@pop-os.localdomain>
2023-03-29 19:56:06 +0100

    few changes

--- src/components/registeredcomptable/popupcrd.js
+++ src/components/registeredcomptable/popupcrd.js
@@ -2,40 +2,49 @@ import avatar from '../../assets/avatar.svg'
 import { AiOutlineCloseCircle } from "react-icons/ai";
 const InputCard = (props) => {
   return (
-		<div className="popcrdelmnts">
-			<div className="popupcrd">
-				<div className="popupheadr">
-					<AiOutlineCloseCircle className="close" onClick={()=> props.setModal(false)} />
-					<img src={avatar} alt="" />
-				</div>
-				<div className="inputelmnts">
-					<div className="inputname">
-						<label>
-							<h1>First name</h1>
-							<input type="text" placeholder="Victony" />
-						</label>
-						<label>
-							<h1>Last name</h1>
-							<input type="text" placeholder="Victony" />
-						</label>
+		<div className="modal">
+			<div className="popcrdelmnts">
+				<div className="popupcrd">
+					<div className="popupheadr">
+						<AiOutlineCloseCircle
+							className="close"
+							onClick={() => props.setModal(false)}
+						/>
+						<img src={avatar} alt="" />
 					</div>
-					<div className="email">
-						<label>
-							<h1>Email</h1>
-							<input type="email" placeholder="Victony@gmail.com" id="email" />
-						</label>
-					</div>
-					<div className="gender">
-						<label>
-							<h1>Gender</h1>
-							<input type="gender" placeholder="Male" id="gender" />
-						</label>
-					</div>
-					<div className="date">
-						<label>
-							<h1>Date of Registration</h1>
-							<input type="date" placeholder="10 / 20/ 2021" id="date" />
-						</label>
+					<div className="inputelmnts">
+						<div className="inputname">
+							<label>
+								<h1>First name</h1>
+								<input type="text" placeholder="Victony" />
+							</label>
+							<label>
+								<h1>Last name</h1>
+								<input type="text" placeholder="Victony" />
+							</label>
+						</div>
+						<div className="email">
+							<label>
+								<h1>Email</h1>
+								<input
+									type="email"
+									placeholder="Victony@gmail.com"
+									id="email"
+								/>
+							</label>
+						</div>
+						<div className="gender">
+							<label>
+								<h1>Gender</h1>
+								<input type="gender" placeholder="Male" id="gender" />
+							</label>
+						</div>
+						<div className="date">
+							<label>
+								<h1>Date of Registration</h1>
+								<input type="date" placeholder="10 / 20/ 2021" id="date" />
+							</label>
+						</div>
 					</div>
 				</div>
 			</div>
