6151331698c38eaf3cbb7e4326a59e40d931776b
wutche <wutche@pop-os.localdomain>
2023-03-30 11:03:14 +0100

    done with registered company modal

---
+++ src/components/registereduser/modal.js
@@ -0,0 +1,65 @@
+/** @format */
+
+import { AiOutlineCloseCircle } from "react-icons/ai";
+import avatar from "../../assets/avatar.svg";
+import moment from "moment";
+
+const RegModal = (props) => {
+	return (
+		<div className="modal">
+			<div className="modalelmnts">
+				<div className="modalcrd">
+					<div className="popupheadr">
+						<AiOutlineCloseCircle
+							className="close"
+							onClick={() => props.setModal(false)}
+						/>
+						<img src={avatar} alt="" />
+					</div>
+					<div className="inputelmnts">
+						<div className="inputname">
+							<label>
+								<h1>Company Name</h1>
+								<input
+									type="text"
+									placeholder={props.user.name.split(" ")[0]}
+								/>
+							</label>
+						</div>
+						<div className="email">
+							<label>
+								<h1>Email</h1>
+								<input type="email" placeholder={props.user.email} id="email" />
+							</label>
+						</div>
+						<div className="subtype">
+							<label>
+								<h1>Subscription type</h1>
+								<input
+									type="text"
+									placeholder={props.user.substype}
+									id="subscription"
+								/>
+							</label>
+						</div>
+						<div className="date">
+							<label>
+								<h1>Duration</h1>
+								<input
+									type="date"
+									//   placeholder={props.user.date}
+									id="date"
+									value={moment(props.user.rawDate).format(
+										"10/20/2021 - 11/20/2022",
+									)}
+									onChange={() => {}}
+								/>
+							</label>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+export default RegModal;
